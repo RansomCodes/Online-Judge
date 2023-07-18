217da85e8361f3aa28ae2cdfcef9b007785a37af
RansomCodes <manmeet7210@gmail.com>
2023-07-19 01:18:53 +0530

    Java compiler done

--- problems/views.py
+++ problems/views.py
@@ -23,38 +23,51 @@ def submit(request):
     code=request.POST["code"]
     language=request.POST["language"]
     ques_id=request.POST["ques_id"]
-    
-    # Path where the file is stored and needs to be rewritten
-    file_path="assets/user_code.cpp"
-    
-    # Rewriting the file
-    with open(file_path,'w') as file:
-        file.write(code)
-    
     tc=testcases.objects.filter(question_id=ques_id)
     verdict="Accepted"
-    for test in tc:
-        input_data=test.input
-        # print(test.input)
-        output=run_cpp_file("user_code.cpp",input_data)
-        if output.strip()!=test.expected_output.strip():
-            # print(output,"&&&&&&&&&&",test.expected_output)
-            # print(len(output))
-            # print(len(test.expected_output))
-            verdict="Wrong answer"
-            break
+    if language== "option1":
+        # Path where the file is stored and needs to be rewritten
+        file_path="assets/user_code.cpp"
+        
+        # Rewriting the file
+        with open(file_path,'w') as file:
+            file.write(code)
+        
+        # Getting hold of every testcase associated with the current question id
+        for test in tc:
+            input_data=test.input
+            # print(test.input)
+            output=run_cpp_file("user_code.cpp",input_data)
+            if output.strip()!=test.expected_output.strip():
+                # print(output,"&&&&&&&&&&",test.expected_output)
+                # print(len(output))
+                # print(len(test.expected_output))
+                verdict="Wrong answer"
+                break
+            
+    elif language=="option2":
+        file_path="assets/AddTwoNumbers.java"
+        with open(file_path,"w") as file:
+            file.write(code)
+        for test in tc:
+            input_data=test.input
+            output=run_java_file("AddTwoNumbers.java",input_data)
+            if output.strip()!=test.expected_output.strip():
+                verdict="Wrong Answer"
+                break
     return render(request,'verdict.html',{'verdict':verdict})
+
 
 def run_cpp_file(file_name,input_data):
     # connect the file name to its correct path
     file_path=os.path.join("assets",file_name)
     
-    # Check if the file actually exists or not
     # if not os.path.exists(file_path):
     #     print("File not found")
-    # with open(file_path, 'r') as file:
-    #     content = file.read()
-    #     print("$$$$$$$$", content)
+    # else:
+    #     with open(file_path,"r") as file:
+    #         content=file.read()
+    #         print(content)
         
     # Compiling the file with user input
     output_file_name="compiled_output"
@@ -67,3 +80,26 @@ def run_cpp_file(file_name,input_data):
             return run_result.stderr
     else:
         return compile_result.stderr
+
+
+def run_java_file(file_name,input_data):
+    file_path=os.path.join("assets",file_name)
+    
+    # if not os.path.exists(file_path):
+    #     print("File not found")
+    # else:
+    #     with open(file_path,"r") as file:
+    #         content=file.read()
+    #         print(content)
+    
+    compile_result=subprocess.run(["javac",file_path],capture_output=True,text=True)
+    if compile_result.returncode==0:
+        class_name= os.path.splitext(file_name)[0] #Extract the class name from the file name
+        run_result=subprocess.run(["java","-classpath","assets",class_name],input=input_data,capture_output=True,text=True)
+        if run_result.returncode==0:
+            return run_result.stdout
+        else:
+            return run_result.stderr
+    else:
+        return compile_result.stderr
+    
--- user_java_code.java
+++ user_java_code.java
@@ -3,10 +3,16 @@ import java.util.Scanner;
 public class AddTwoNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int num1 = scanner.nextInt();;
+
+        System.out.print("Enter the first number: ");
+        int num1 = scanner.nextInt();
+
+        System.out.print("Enter the second number: ");
         int num2 = scanner.nextInt();
+
         int sum = num1 + num2;
-        System.out.println(sum);
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+
         scanner.close();
     }
 }
