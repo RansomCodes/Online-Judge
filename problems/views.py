86818b60c9f694c70ddcf2ef47546a71689d2a90
RansomCodes <manmeet7210@gmail.com>
2023-07-18 22:10:18 +0530

    Added C++ Submission Function

--- problems/views.py
+++ problems/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import questions
+import subprocess,os
 
 # Create your views here.
 def home(request):
@@ -17,4 +18,35 @@ def question_detail(request,question_id):
 
 
 def submit(request):
-    return render(request,'verdict.html')
+    code=request.POST["code"]
+    language=request.POST["language"]
+    file_path="assets/user_code.cpp"
+    output=""
+    with open(file_path,'w') as file:
+        file.write(code)
+    input_data="50"
+    output=run_cpp_file("user_code.cpp",input_data)
+    return render(request,'verdict.html',{'output':output})
+
+def run_cpp_file(file_name,input_data):
+    # connect the file name to its correct path
+    file_path=os.path.join("assets",file_name)
+    
+    # Check if the file actually exists or not
+    if not os.path.exists(file_path):
+        print("File not found")
+    with open(file_path, 'r') as file:
+        content = file.read()
+        print("$$$$$$$$", content)
+        
+    # Compiling the file with user input
+    output_file_name="compiled_output"
+    compile_result=subprocess.run(["g++",file_path,"-o",output_file_name],capture_output=True,text=True)
+    if compile_result.returncode == 0:
+        run_result=subprocess.run(["./"+output_file_name],input=input_data,capture_output=True,text=True)
+        if run_result.returncode==0:
+            return run_result.stdout
+        else:
+            return run_result.stderr
+    else:
+        return compile_result.stderr
